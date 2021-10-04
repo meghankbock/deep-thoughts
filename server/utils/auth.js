a97239a03ee0f4b762c284c954d4ad11087d7a60
Meghan Bock <meghan.bock@gmail.com>
2021-10-03 22:21:03 -0500

    set up user signup and user login including front-end authorization

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,20 +1,15 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-const secret = process.env.SECRET_KEY;
-const expiration = process.env.EXPIRATION;
+const secret = process.env.SET_SECRET_KEY;
+const expiration = process.env.SET_EXPIRATION;
 
 module.exports = {
-    signToken: function({ username, email, _id }) {
-        const payload = { username, email, _id };
-
-        return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-    },
     authMiddleware: function({ req }) {
         let token = req.body.token || req.query.token || req.headers.authorization;
 
         if (req.headers.authorization) {
-            token = token.split('').pop().trim();
+            token = token.split(' ').pop().trim();
         }
 
         if (!token) {
@@ -29,5 +24,10 @@ module.exports = {
         }
 
         return req;
+    },
+    signToken: function({ username, email, _id }) {
+        const payload = { username, email, _id };
+
+        return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
     }
 };
