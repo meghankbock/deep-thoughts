75d4f863aafcec4c70df829384a2a6336bfaa49c
Meghan Bock <meghan.bock@gmail.com>
2021-10-03 19:02:36 -0500

    set up react app

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const { ApolloServerPluginLandingPageGraphQLPlayground } = require('apollo-server-core');
 const { authMiddleware } = require('./utils/auth');
+const path = require('path');
 
 // import typeDefs and resolvers
 const { typeDefs, resolvers } = require("./schemas");
@@ -38,6 +39,14 @@ startServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once("open", () => {
   app.listen(PORT, () => {
